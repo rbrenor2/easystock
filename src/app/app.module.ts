4c5003582b0c1adf4e28eb8553cbba2af5ae47e7
brenorios-br2digital <rbrenorios@gmail.com>
2023-12-25 11:50:50 -0300

    Adding PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -12,10 +12,16 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getPerformance, providePerformance } from '@angular/fire/performance';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({ "projectId": "easystock-c61a7", "appId": "1:881269434041:web:407c8328b0bede1cecdd68", "storageBucket": "easystock-c61a7.appspot.com", "apiKey": "AIzaSyDsoPwTq3GZxEPqEIh--tKz_GeirMBsigY", "authDomain": "easystock-c61a7.firebaseapp.com", "messagingSenderId": "881269434041", "measurementId": "G-F58QPFY47W" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), providePerformance(() => getPerformance())],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({ "projectId": "easystock-c61a7", "appId": "1:881269434041:web:407c8328b0bede1cecdd68", "storageBucket": "easystock-c61a7.appspot.com", "apiKey": "AIzaSyDsoPwTq3GZxEPqEIh--tKz_GeirMBsigY", "authDomain": "easystock-c61a7.firebaseapp.com", "messagingSenderId": "881269434041", "measurementId": "G-F58QPFY47W" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), providePerformance(() => getPerformance()), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
